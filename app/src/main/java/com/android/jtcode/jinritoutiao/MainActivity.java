74f6c8f834861ed6bb4af61c7e8d47fef83462a9
gumenghao <menghao.gu@leediancn.com>
2018-03-29 10:02:10 +0800

    第一次提交

--- app/src/main/java/com/android/jtcode/jinritoutiao/MainActivity.java
+++ app/src/main/java/com/android/jtcode/jinritoutiao/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements ChannelAdapter.on
     private RecyclerView mRecyclerView;
     private List<ChannelBean> mList;
     private ChannelAdapter mAdapter;
+
     private String select[] = {"要闻", "体育", "新时代", "汽车", "时尚", "国际", "电影", "财经", "游戏", "科技", "房产", "政务", "图片", "独家"};
     private String recommend[] = {"娱乐", "军事", "文化", "视频", "股票", "动漫", "理财", "电竞", "数码", "星座", "教育", "美容", "旅游"};
     private String city[] = {"重庆", "深圳", "汕头", "东莞", "佛山", "江门", "湛江", "惠州", "中山", "揭阳", "韶关", "茂名", "肇庆", "梅州", "汕尾", "河源", "云浮", "四川"};
